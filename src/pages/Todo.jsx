f666c5b519e878f6e50063e6341b37319806d5e6
Zafchiel <bialakropla127.9@o2.pl>
2023-04-11 20:17:44 +0200

    adding tasks

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -2,10 +2,14 @@ import supabase from "../services/supabase"
 import { useEffect, useState } from "react"
 import ListItem from "../components/ListItem"
 import Spinner from "../components/Spinner"
+import { useAuthStatus } from "../hooks/useAuthStatus"
 
 function Todo() {
+  const [taskText, setTaskText] = useState("")
   const [tasks, setTasks] = useState([])
   const [loading, setLoading] = useState(true)
+
+  const { session } = useAuthStatus()
 
   // Logout user
   const handleLogout = async () => {
@@ -24,7 +28,22 @@ function Todo() {
     }
 
     fetchTasks()
-  }, [])
+  }, [tasks])
+
+  // Handle adding task
+  const addTask = async (e) => {
+    e.preventDefault()
+    try {
+      const { data } = await supabase.from("todos").insert({
+        task: taskText,
+        is_completed: false,
+        user_id: session.user.id,
+      })
+      console.log(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   if (loading) {
     return <Spinner />
@@ -39,10 +58,22 @@ function Todo() {
       >
         LogOut
       </button>
-      <div className="flex flex-col gap-5">
-        {tasks.map((task, index) => (
-          <ListItem key={index} task={task} />
-        ))}
+      <div className="flex flex-col gap-10">
+        <div>
+          <form onSubmit={addTask}>
+            <input
+              type="text"
+              value={taskText}
+              onChange={(e) => setTaskText(e.target.value)}
+            />
+            <button type="submit">Add</button>
+          </form>
+        </div>
+        <div className="flex flex-col gap-5">
+          {tasks?.map((task, index) => (
+            <ListItem key={index} task={task} />
+          ))}
+        </div>
       </div>
     </div>
   )
